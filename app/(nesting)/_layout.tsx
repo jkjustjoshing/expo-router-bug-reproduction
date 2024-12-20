e74f5f823c230bd8888ed2c9291331ce48b4f439
Josh Kramer <josh@almondobgyn.com>
2024-12-19 22:42:10 -0500

    Simplify with one fewer level of nesting

--- app/(nesting)/_layout.tsx
+++ app/(nesting)/_layout.tsx
@@ -1,6 +1,22 @@
-import { Stack } from "expo-router";
-import React from "react";
+import { Stack, usePathname, useSegments } from "expo-router";
+import React, { useState } from "react";
+import { Button, Text } from "react-native";
 
 export default () => {
+  const [isReady, setIsReady] = useState(false);
+
+  console.log({ segments: useSegments(), pathname: usePathname() });
+  if (!isReady) {
+    return (
+      <>
+        <Text>
+          Waiting to render the &lt;Stack&gt; from
+          /(nesting)/(second)/_layout.tsx until pressing this button
+        </Text>
+        <Button title="Press me" onPress={() => setIsReady(true)} />
+      </>
+    );
+  }
+
   return <Stack />;
 };
